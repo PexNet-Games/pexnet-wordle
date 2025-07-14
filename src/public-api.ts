f148ddbbb5c6cb5ec14ac0944a9cb4d2d1d04247
Gaël <67436391+Pexilo@users.noreply.github.com>
2025-07-14 16:37:24 +0200

    feat: add public API surface for pexnet-wordle

---
+++ src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of pexnet-wordle
+ */
+
+export * from "./app/wordle/wordle.component";
+export * from "./app/wordle/wordle.service";
+export * from "./app/instructions-modal/instructions-modal.component";
+export * from "./app/pop-up/pop-up.component";
+export * from "./app/utils/accent-utils";
